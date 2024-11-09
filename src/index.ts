e91ef554e642d5cf78c90e81e6ead6ed978686e7
andyrhman <andyrhmann@gmail.com>
2024-11-09 22:02:30 +0800

    push ke 3

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,5 @@ app.use(express.json()); // ! INI SELALU HARUS ADA DIATAS :)
 routes(app); // ! INi Kode
 
 app.listen(8000, () => {
-    console.log("Server berhasil jalan");
+    console.log("Server berhasil jalan"); // ! Ini kode server berjalan
 });
